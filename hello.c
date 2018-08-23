9a5891ef7eca28c8584ea9a8e4e0c3275e42b610
David Estrada <estradadae6@gmail.com>
2018-08-23 13:02:09 -0500

    Name and Major print out

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: David Estrada
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("David Estrada\nComputer Engineering");
+
+  return 0;
+}
